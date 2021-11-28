1d581ad79af36ee3b87b8790590c9a78c9c98540
yakov <iakovboiev@gmail.com>
2021-11-28 19:08:43 +0200

    create static web templates

--- department-app/app.py
+++ department-app/app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/index')
 @app.route('/')
 def index():
-    return 'index Epam'
+    return render_template('index.html')
+
+
+@app.route('/departments')
+def departments():
+    return render_template('departments.html')
+
+
+@app.route('/employees')
+def employees():
+    return render_template('employees.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
